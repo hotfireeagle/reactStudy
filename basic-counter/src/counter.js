7f4937e7cd387c9b4ab891676ec44de8bf1928b8
hotfireeagle <hotfireeagle@163.com>
2017-09-07 20:51:13 +0800

    basic counter

---
+++ basic-counter/src/counter.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react"
+
+let buttonStyle = {
+	"display": "inline-block",
+	"width": "70px",
+	"height": "70px",
+	"lineHeight": "70px",
+	"borderRadius": "70px",
+	"color": "#fff",
+	"fontSize": "50px",
+	"fontWeight": "bolder",
+	"textAlign": "center",
+	"backgroundColor": "#7b72e9",
+	"border": "1px solid #7b72e9",
+	"marginRight": "50px",
+	"marginLeft": "50px"
+}
+
+let contentStyle = {
+	"display": "inline-block",
+	"width": "90px",
+	"height": "70px",
+	"lineHeight": "70px",
+	"textAlign": "center",
+	"marginTop": 0
+}
+
+class Counter extends Component{
+	constructor(props){
+		super(props)
+		this.incrementClick = this.incrementClick.bind(this)
+		this.decrementClick = this.decrementClick.bind(this)
+		this.state = {
+			"times": 0
+		}
+	}
+
+	incrementClick(ev){
+		this.setState({"times": this.state.times+1})
+	}
+
+	decrementClick(ev){
+		this.setState({"times": this.state.times-1})
+	}
+
+	render(){
+		return (
+			<div>
+				<button style={buttonStyle} onClick={this.incrementClick}>+</button>
+				<span style={contentStyle}>{this.state.times}</span>
+				<button style={buttonStyle} onClick={this.decrementClick}>-</button>
+			</div>
+		)
+	}
+}
+
+export default Counter
